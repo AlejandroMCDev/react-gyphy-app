21f6ed737391ad51e96105dfdf127c43b244d9bf
Josean <alejandrowowsage@gmail.com>
2023-03-28 14:54:25 -0500

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import {AddCategory,GifGrid} from "./components";
+
+
+
+
+const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Fire Emblem']);
+
+    const onAddCategory = (newCategory) =>{
+
+      if (categories.includes(newCategory)) {
+        console.warn("Es un item repetido");
+        return;
+      }
+
+      setCategories([newCategory,...categories]);
+    }
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+      <AddCategory 
+        //setCategory = {setCategories}
+        onNewCategory = {onAddCategory}
+      />
+        {categories.map( category => <GifGrid key={category} category={category}/> )}
+    </>
+  )
+}
+
+export default GifExpertApp;
